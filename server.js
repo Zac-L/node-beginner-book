78e62b067afc19d4b4a68e99568f0fa940332f22
Zac-L <Zachlichens@gmail.com>
2017-10-16 10:57:19 -0700

    setup export of server file

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const http = require('http');
 
-function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('hello world');
-    response.end();
-    
-}
 
-http.createServer(onRequest).listen(8888);
+function start() {
+    function onRequest(request, response) {
+        console.log('Request received'); // eslint-disable-line
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write('hello world');
+        response.end();
+        
+    }
+
+    http.createServer(onRequest).listen(8888);
 
 console.log('Node Server is running!'); // eslint-disable-line
+}
+
+module.exports = start;
