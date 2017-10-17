3c3d5237cbe3621542d2e032e7d6936288029456
Zac-L <Zachlichens@gmail.com>
2017-10-16 23:18:21 -0700

    updated routes to handle request instead of postData

--- server.js
+++ server.js
@@ -5,20 +5,10 @@ const url = require('url');
 
 function start(route, handle) {
     function onRequest(request, response) {
-        let postData = '';
         const pathname = url.parse(request.url).pathname;
         console.log('Request for ' + pathname + ' received'); // eslint-disable-line
 
-        request.setEncoding('utf8');
-        
-        request.addListener('data', postDataChunk => {
-            postData += postDataChunk;
-            console.log('Received POST data chunk ' + postDataChunk + '.');
-        });
-
-        request.addListener('end', () => {
-            route(handle, pathname, response, postData);
-        });
+        route(handle, pathname, response, request);
 
     }
 
